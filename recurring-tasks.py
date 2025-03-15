ff31b675d8f990d4b5f607ccc703978d4a928d40
rha <Stonework1547@floundersmail.com>
2025-03-15 10:29:25 -0700

    first commit

--- recurring-tasks.py
+++ recurring-tasks.py
@@ -1,32 +1,33 @@
-# Create list of Networking Breakfast Meeting calendar items
+# Create list of recurring todotxt tasks
 #
 # Write output to a csv.
 import datetime
 
-meetingName = "Networking Breakfast Meeting"
-number_of_meetings = 6
+priority = "(B)"
+number_of_recurrences = 10
 
 # set start date
 year = 2025
-month = 2
-day = 2
+month = 4
+day = 1
 start_date = datetime.date(year, month, day)
 
-start_time = "7:00 AM"
-end_time = "9:00 AM"
-all_day_event = "False"
-description = ""
-location = " St Timothy Lutheran Church 1051 Kempsville Rd Norfolk VA 23502"
-private = "False"
+description = "Replace toothbrush head"
+creation_date = "2025-03-15"
+end_time = ""
+all_day_event = ""
+context = ""
+project = ""
 
-path = 'G:\\OneDrive\\Desktop\\chapter_calendar_nbm.csv'
+path = 'G:\\OneDrive\\Desktop\\recurring-tasks.txt'
 with open(path, 'w') as outfile:
-    outfile.write(str("Subject,Start Date,Start Time,End Date,End Time,All Day Event,Description,Location,Private\n"))
-    for i in range(number_of_meetings):
-        end_date = start_date
+#    comment to remove column headers
+#    outfile.write(str("Subject,Start Date,Start Time,End Date,End Time,All Day Event,Description,Location,Private\n"))
+    for i in range(number_of_recurrences):
+        threshold_date = start_date
         month1 = start_date.month
-        outfile.write("{0},{1},{2},{3},{4},{5},{6},{7},{8}\n".format(meetingName, start_date, start_time, end_date, end_time, all_day_event, description, location, private))
-        start_date = start_date + datetime.timedelta(weeks=4)
+        outfile.write("{0} {1} {2} due:{3} t:{4} {5} {6}\n".format(priority, creation_date, description, start_date, threshold_date, project, context))
+        start_date = start_date + datetime.timedelta(weeks=6)
         month2 = start_date.month
-        if month2 == month1:
-            start_date = start_date + datetime.timedelta(days=7)
+        #if month2 == month1:
+        #   start_date = start_date + datetime.timedelta(days=7)
